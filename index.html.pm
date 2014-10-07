24e79383f4d24d5e1549e5c0f3a9f175c00bc6a5
nvanhorn <nemo1211@gmail.com>
2014-10-07 16:05:19 -0400

    Initial commit

---
+++ index.html.pm
@@ -0,0 +1,46 @@
+#lang pollen
+
+◊h3{Home} 
+
+◊p{MassMine is a social media mining and archiving application that simplifies the process of collecting and managing large amounts of data across multiple sources. It is designed with the researcher in mind, providing a flexible framework for tackling individualized research needs. MassMine is designed to run both on personal computers and dedicated servers/clusters. MassMine handles credential authorizations, data acquisition & archiving, as well as customized data export and analysis.}
+
+◊h3{With MassMine You Can:}
+
+◊ul['class:""]{
+
+◊li{Automate access to social media and web data through a flexible interface. MassMine runs on personal computers and dedicated servers.} 
+◊br{}
+◊li{Collect current and past data from sources such as Twitter, Facebook, google trends, and Wikipedia. Your data is stored on your own machine.} 
+◊br{}
+◊li{Process and analyze your data to satisfy exploratory and targeted investigations of social media trends and network connectivity.} 
+
+}
+
+◊h3{Download}
+
+◊p{MassMine can be installed and used free of charge. Downloadable files and installation instructions are currently available at the official ◊a['href: "https://github.com/n3mo/massmine"]{github repository}.}
+
+◊h3{Contact}
+
+◊p{Questions, comments, and bug reports should be directed at the MassMine ◊a['href: "https://github.com/n3mo/massmine/issues"]{issue tracker}, and feature requests at the ◊a['href: "https://github.com/n3mo/massmine/pulls"]{pull request page}.}
+
+
+◊; Sidebar information
+◊div['id: "primary" 'class: "sidebar"]{
+◊ul['class:"nobullet"]{
+◊li{◊a['href:"/index.html"]{Home}}
+◊li{◊a['href:"https://github.com/n3mo/massmine"]{Github Repo}}
+◊li{◊a['href:"/docs/index.html"]{Documentation}}
+◊br{}
+◊li{Collaborators
+    ◊dl['class:"nobullet"]{
+      ◊dt{◊a['href:"http://www.nicholasvanhorn.com"]{Nicholas Van Horn}}
+      ◊dd{Co-creator and project programmer} 
+      ◊dt{◊a['href:"http://www.aaronbeveridge.com"]{Aaron Beveridge}}
+      ◊dd{Co-creator and project developer} 
+
+}
+
+}
+}}
+

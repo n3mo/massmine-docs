ecac9c441d37276cb3201e90bf67d1d3aed56e1c
nvanhorn <nemo1211@gmail.com>
2014-10-07 19:16:08 -0400

    Moved sidebar to template. Fixed broken links

--- docs/index.html.pm
+++ docs/index.html.pm
@@ -3,17 +3,3 @@
 ◊h2{Documentation}
 
 Installation instructions can currently be found at the ◊a['href:"https://github.com/n3mo/massmine"]{MassMine Github repository}. 
-
-◊; Sidebar information
-◊div['id: "primary" 'class: "sidebar"]{
-◊ul['class:"nobullet"]{
-◊li{◊a['href:"/index.html"]{Home}}
-◊li{◊a['href:"https://github.com/n3mo/massmine"]{Github Repo}}
-◊li{◊a['href:"/docs/index.html"]{Documentation}}
-◊br{}
-◊li{Collaborators
-    ◊dl['class:"nobullet"]{
-      ◊dt{◊a['href:"http://www.nicholasvanhorn.com"]{Nicholas Van Horn}}
-      ◊dd{Co-creator and project programmer} 
-      ◊dt{◊a['href:"http://www.aaronbeveridge.com"]{Aaron Beveridge}}
-      ◊dd{Co-creator and project developer}}}}}
--- index.html.pm
+++ index.html.pm
@@ -25,22 +25,4 @@
 ◊p{Questions, comments, and bug reports should be directed at the MassMine ◊a['href: "https://github.com/n3mo/massmine/issues"]{issue tracker}, and feature requests at the ◊a['href: "https://github.com/n3mo/massmine/pulls"]{pull request page}.}
 
 
-◊; Sidebar information
-◊div['id: "primary" 'class: "sidebar"]{
-◊ul['class:"nobullet"]{
-◊li{◊a['href:"/index.html"]{Home}}
-◊li{◊a['href:"https://github.com/n3mo/massmine"]{Github Repo}}
-◊li{◊a['href:"/docs/index.html"]{Documentation}}
-◊br{}
-◊li{Collaborators
-    ◊dl['class:"nobullet"]{
-      ◊dt{◊a['href:"http://www.nicholasvanhorn.com"]{Nicholas Van Horn}}
-      ◊dd{Co-creator and project programmer} 
-      ◊dt{◊a['href:"http://www.aaronbeveridge.com"]{Aaron Beveridge}}
-      ◊dd{Co-creator and project developer} 
-
-}
-
-}
-}}
 

66592ca2c020419b3c60a80fb88ecc58a18b8bc3
n3mo <nemo1211@gmail.com>
2014-11-14 14:08:43 -0500

    Major updates to docs. Added template, install info

--- docs/index.html.pm
+++
@@ -1,8 +0,0 @@
-#lang pollen
-◊meta['template: "docs_template.html"]
-
-◊h2{Documentation}
-
-Installation instructions can currently be found at the ◊a['href:"https://github.com/n3mo/massmine"]{MassMine Github repository}. 
-
-
--- documentation.html.pm
+++
@@ -1,7 +0,0 @@
-#lang pollen
-
-◊meta['page-title: "MassMine Documentation"]
-
-◊h2{Documentation}
-
-Installation instructions can currently be found at the ◊link["https://github.com/n3mo/massmine"]{MassMine Github repository}. 

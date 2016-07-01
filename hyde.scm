ea022ba39601a05716534eadae7c0a277f09ae05
nemo <nemo1211@gmail.com>
2016-06-30 20:06:43 -0400

    Tinkering with pandoc as md convertor

--- hyde.scm
+++ hyde.scm
@@ -1,7 +1,15 @@
 (use hyde)
 (use lowdown)
 
+;; (define translate-markdown
+;;   (make-external-translator
+;;    "pandoc"
+;;    (lambda () '("-f" "markdown" "-t" "html5" "--toc"))))
+;; (translators (cons (list "md" translate-markdown) (translators)))
+
+
 (translators (cons (list "md" markdown->html) (translators)))
+
 
 
 
